ddaf54450cb92fa7dc5cf64b01da0992d4606f8b
Fedor <f.a.orlov@gmail.com>
2022-07-08 13:01:32 +0300

    My post

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,8 @@ class StaticPagesController < ApplicationController
  def about
  end
 
+ def logout
+ end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
-  
   devise_for :users
   root 'static_pages#home'
 
+  get '/posts', to: 'posts#index'
+
+
   get '/about', to: 'static_pages#about'
 
-  # get login
+  
 
 
 end
