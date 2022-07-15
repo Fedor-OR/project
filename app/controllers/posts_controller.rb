0a82ebc001c325629857998f5093cb5f942d4242
Fedor <f.a.orlov@gmail.com>
2022-07-15 10:52:15 +0300

    Image post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
     private
 
      def post_params
-        params.require(:post).permit(:title, :body, :comments)
+        params.require(:post).permit(:title, :body, :comments, :photo)
      end    
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     belongs_to :user 
     has_many :likes, dependent: :destroy
+    has_one_attached :photo
     # validates :author, presense: true
     # after_commit :post_created_mail, on: :create
     # after_commit :post_updated_mail, on: :update
