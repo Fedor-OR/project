10c23c5816409aaf7c466249a1921c1523cf63e6
Fedor <f.a.orlov@gmail.com>
2023-03-29 14:17:25 +0300

    tests controllers and models

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe LikesController, type: :controller do
 
     subject { process :create, method: :post, params: params }
 
-    let(:params)  { { like: { post_id: post.id, user_id: user.id } } } 
+    let(:params)  { { post_id: post.id, user_id: user.id } }  
     let!(:post) { create :post, user: user } 
 
 
@@ -20,19 +20,23 @@ RSpec.describe LikesController, type: :controller do
 
   end
 
-    # it "отображенеи количества лайков" do
-    # subject
-    # expect(assigns(:likes)).to eq(likes)
-    # expect(assigns(:products).count).to eq 1
-    # expect(assigns(:products).count).to be_positive
 
-    # end
+  describe "#delete" do
+
+      subject { process :destroy, method: :delete, params: params } 
+  
+      let!(:like) { create :like, post: post, user: user }
+      # let(:params) { { id: { post_id: post.id, user_id: user.id } } }
+      let!(:post) { create :post, user: user } 
+      let(:params) { { post_id: post.id, id: like.id } }
 
 
 
-    # it "убрать лайк" do
-    
-    # end
+      
+    it "убрать лайк" do
+      expect { subject }.to change(Like, :count).by(-1)
+    end
 
+  end
 
 end
